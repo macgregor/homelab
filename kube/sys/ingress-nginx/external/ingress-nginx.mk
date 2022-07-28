65699660a58d972329a6a709e4629ba213efd824
Matthew Stratton <mstratto@redhat.com>
2022-07-28 12:29:39 -0400

    fixing leader election between int/ext loadbalancers (they were using the same id), making sure they request the correct lan ip address for LB service

--- kube/sys/ingress-nginx/external/ingress-nginx.mk
+++ kube/sys/ingress-nginx/external/ingress-nginx.mk
@@ -1,3 +1,4 @@
+INGRESS_EXTERNAL_REPLICAS?=1
 
 .PHONY: ingress-external-deploy
 ingress-external-deploy:
@@ -6,3 +7,43 @@ ingress-external-deploy:
 .PHONY: ingress-external-remove
 ingress-external-remove:
 	helmfile --file ./sys/ingress-nginx/external/helmfile.yaml destroy
+
+.PHONY: ingress-external-debug
+ingress-external-debug:
+	kubectl -n ingress-nginx exec -it `kubectl -n ingress-nginx get pods -l app.kubernetes.io/instance=ingress-nginx-external -o name | head -n 1` -- bash
+
+.PHONY: ingress-external-logs
+ingress-external-logs:
+	kubectl -n ingress-nginx logs `kubectl -n ingress-nginx get pods -l app.kubernetes.io/instance=ingress-nginx-external -o name | head -n 1` --follow
+
+.PHONY: ingress-external-stop
+ingress-external-stop:
+	kubectl -n ingress-nginx scale deployment/ingress-nginx-external-controller-external --replicas=0
+
+.PHONY: ingress-external-start
+ingress-external-start:
+	kubectl -n ingress-nginx scale deployment/ingress-nginx-external-controller-external --replicas=${INGRESS_EXTERNAL_REPLICAS}
+
+.PHONY: ingress-external-restart
+ingress-external-restart:
+	kubectl -n ingress-nginx rollout restart deployment/ingress-nginx-external-controller-external
+
+.PHONY: ingress-external-status
+ingress-external-status:
+	@echo "======================================================================================"
+	@echo "= ingress-external Network Resources:                                                            ="
+	@echo "=   kubectl -n ingress-nginx get svc,endpoints,ingress -l app.kubernetes.io/instance=ingress-nginx-external'        ="
+	@echo "======================================================================================"
+	@kubectl -n ingress-nginx get svc -l 'app.kubernetes.io/instance=ingress-nginx-external' -o custom-columns=NAME:.metadata.name,TYPE:.spec.type,CLUSTER-IP:.spec.clusterIP,EXTERNAL-IP:.spec.loadBalancerIP
+	@echo ""
+	@kubectl -n ingress-nginx get endpoints,ingress -l 'app.kubernetes.io/instance=ingress-nginx-external'
+	@echo "\n======================================================================================"
+	@echo "= ingress-external Storage Resources:                                                            ="
+	@echo "=   kubectl -n ingress-nginx get pvc -l 'app.kubernetes.io/instance=ingress-nginx-external'                         ="
+	@echo "======================================================================================"
+	@kubectl -n ingress-nginx get pvc -l 'app.kubernetes.io/instance=ingress-nginx-external' -o custom-columns=NAME:.metadata.name,STATUS:.status.phase,VOLUME:.spec.volumeName
+	@echo "\n======================================================================================"
+	@echo "= ingress-external Deployment Resources:                                                         ="
+	@echo "=   kubectl -n ingress-nginx get deployment,rs,pods -l 'app.kubernetes.io/instance=ingress-nginx-external'          ="
+	@echo "======================================================================================"
+	@kubectl -n ingress-nginx get deployment,rs,pods -l 'app.kubernetes.io/instance=ingress-nginx-external'
--- kube/sys/ingress-nginx/internal/ingress-nginx.mk
+++ kube/sys/ingress-nginx/internal/ingress-nginx.mk
@@ -1,3 +1,4 @@
+INGRESS_INTERNAL_REPLICAS?=1
 
 .PHONY: ingress-internal-deploy
 ingress-internal-deploy:
@@ -6,3 +7,43 @@ ingress-internal-deploy:
 .PHONY: ingress-internal-remove
 ingress-internal-remove:
 	helmfile --file ./sys/ingress-nginx/internal/helmfile.yaml destroy
+
+.PHONY: ingress-internal-debug
+ingress-internal-debug:
+	kubectl -n ingress-nginx exec -it `kubectl -n ingress-nginx get pods -l app.kubernetes.io/instance=ingress-nginx-internal -o name | head -n 1` -- bash
+
+.PHONY: ingress-internal-logs
+ingress-internal-logs:
+	kubectl -n ingress-nginx logs `kubectl -n ingress-nginx get pods -l app.kubernetes.io/instance=ingress-nginx-internal -o name | head -n 1` --follow
+
+.PHONY: ingress-internal-stop
+ingress-internal-stop:
+	kubectl -n ingress-nginx scale deployment/ingress-nginx-internal-controller-internal --replicas=0
+
+.PHONY: ingress-internal-start
+ingress-internal-start:
+	kubectl -n ingress-nginx scale deployment/ingress-nginx-internal-controller-internal --replicas=${INGRESS_INTERNAL_REPLICAS}
+
+.PHONY: ingress-internal-restart
+ingress-internal-restart:
+	kubectl -n ingress-nginx rollout restart deployment/ingress-nginx-internal-controller-internal
+
+.PHONY: ingress-internal-status
+ingress-internal-status:
+	@echo "======================================================================================"
+	@echo "= ingress-internal Network Resources:                                                            ="
+	@echo "=   kubectl -n ingress-nginx get svc,endpoints,ingress -l app.kubernetes.io/instance=ingress-nginx-internal'        ="
+	@echo "======================================================================================"
+	@kubectl -n ingress-nginx get svc -l 'app.kubernetes.io/instance=ingress-nginx-internal' -o custom-columns=NAME:.metadata.name,TYPE:.spec.type,CLUSTER-IP:.spec.clusterIP,EXTERNAL-IP:.spec.loadBalancerIP
+	@echo ""
+	@kubectl -n ingress-nginx get endpoints,ingress -l 'app.kubernetes.io/instance=ingress-nginx-internal'
+	@echo "\n======================================================================================"
+	@echo "= ingress-internal Storage Resources:                                                            ="
+	@echo "=   kubectl -n ingress-nginx get pvc -l 'app.kubernetes.io/instance=ingress-nginx-internal'                         ="
+	@echo "======================================================================================"
+	@kubectl -n ingress-nginx get pvc -l 'app.kubernetes.io/instance=ingress-nginx-internal' -o custom-columns=NAME:.metadata.name,STATUS:.status.phase,VOLUME:.spec.volumeName
+	@echo "\n======================================================================================"
+	@echo "= ingress-internal Deployment Resources:                                                         ="
+	@echo "=   kubectl -n ingress-nginx get deployment,rs,pods -l 'app.kubernetes.io/instance=ingress-nginx-internal'          ="
+	@echo "======================================================================================"
+	@kubectl -n ingress-nginx get deployment,rs,pods -l 'app.kubernetes.io/instance=ingress-nginx-internal'
