4708486d66ba88b901e872f38dfc54979686f75c
Matthew Stratton <mstratto@redhat.com>
2022-07-28 12:38:12 -0400

    updating demo apps

--- kube/demo/kuard/kuard.mk
+++ kube/demo/kuard/kuard.mk
@@ -14,7 +14,7 @@ kuard-remove:
 
 .PHONY: kuard-debug
 kuard-debug:
-	kubectl -n kuard exec -it `kubectl -n kuard get pods -l app.kubernetes.io/name=kuard -o name | head -n 1` -- bash
+	kubectl -n demo exec -it `kubectl -n demo get pods -l app.kubernetes.io/name=kuard -o name | head -n 1` -- bash
 
 .PHONY: kuard-logs
 kuard-logs:
@@ -22,32 +22,32 @@ kuard-logs:
 
 .PHONY: kuard-stop
 kuard-stop:
-	kubectl -n kuard scale deployment/kuard --replicas=0
+	kubectl -n demo scale deployment/kuard --replicas=0
 
 .PHONY: kuard-start
 kuard-start:
-	kubectl -n kuard scale deployment/kuard --replicas=${KUARD_REPLICAS}
+	kubectl -n demo scale deployment/kuard --replicas=${KUARD_REPLICAS}
 
 .PHONY: kuard-restart
 kuard-restart:
-	kubectl -n kuard rollout restart deployment/kuard
+	kubectl -n demo rollout restart deployment/kuard
 
 .PHONY: kuard-status
 kuard-status:
 	@echo "======================================================================="
 	@echo "= kuard Network Resources:                                         ="
-	@echo "=   kubectl -n kuard get svc,endpoints,ingress -l app.kubernetes.io/name=kuard'    ="
+	@echo "=   kubectl -n demo get svc,endpoints,ingress -l app.kubernetes.io/name=kuard'    ="
 	@echo "======================================================================="
-	@kubectl -n kuard get svc -l 'app.kubernetes.io/name=kuard' -o custom-columns=NAME:.metadata.name,TYPE:.spec.type,CLUSTER-IP:.spec.clusterIP,EXTERNAL-IP:.spec.loadBalancerIP
+	@kubectl -n demo get svc -l 'app.kubernetes.io/name=kuard' -o custom-columns=NAME:.metadata.name,TYPE:.spec.type,CLUSTER-IP:.spec.clusterIP,EXTERNAL-IP:.spec.loadBalancerIP
 	@echo ""
-	@kubectl -n kuard get endpoints,ingress
+	@kubectl -n demo get endpoints,ingress
 	@echo "\n======================================================================="
 	@echo "= kuard Storage Resources:                                          ="
-	@echo "=   kubectl -n kuard get pvc -l 'app=kuard'                      ="
+	@echo "=   kubectl -n demo get pvc -l 'app=kuard'                      ="
 	@echo "======================================================================="
-	@kubectl -n kuard-multitool get pvc -l 'app.kubernetes.io/name=kuard' -o custom-columns=NAME:.metadata.name,STATUS:.status.phase,VOLUME:.spec.volumeName
+	@kubectl -n demo-multitool get pvc -l 'app.kubernetes.io/name=kuard' -o custom-columns=NAME:.metadata.name,STATUS:.status.phase,VOLUME:.spec.volumeName
 	@echo "\n======================================================================="
 	@echo "= kuard Deployment Resources:                                       ="
-	@echo "=   kubectl -n kuard get deployment,rs,pods -l 'app.kubernetes.io/name=kuard'       ="
+	@echo "=   kubectl -n demo get deployment,rs,pods -l 'app.kubernetes.io/name=kuard'       ="
 	@echo "======================================================================="
-	@kubectl -n kuard get deployment,rs,pods -l 'app.kubernetes.io/name=kuard'
+	@kubectl -n demo get deployment,rs,pods -l 'app.kubernetes.io/name=kuard'
--- kube/demo/whoami/whoami.mk
+++ kube/demo/whoami/whoami.mk
@@ -10,11 +10,11 @@ whoami-deploy:
 whoami-remove:
 	-kubectl delete -f ./demo/whoami/whoami.yml
 	-kubectl delete -f ./demo/whoami/network.yml
-	-kubectl delete -f ./demo/whoami/namespace.yml --cascade=background
+	#-kubectl delete -f ./demo/whoami/namespace.yml --cascade=background
 
 .PHONY: whoami-debug
 whoami-debug:
-	kubectl -n whoami exec -it `kubectl -n whoami get pods -l app.kubernetes.io/name=whoami -o name | head -n 1` -- bash
+	kubectl -n demo exec -it `kubectl -n demo get pods -l app.kubernetes.io/name=whoami -o name | head -n 1` -- bash
 
 .PHONY: whoami-logs
 whoami-logs:
@@ -22,32 +22,32 @@ whoami-logs:
 
 .PHONY: whoami-stop
 whoami-stop:
-	kubectl -n whoami scale deployment/whoami --replicas=0
+	kubectl -n demo scale deployment/whoami --replicas=0
 
 .PHONY: whoami-start
 whoami-start:
-	kubectl -n whoami scale deployment/whoami --replicas=${WHOAMI_REPLICAS}
+	kubectl -n demo scale deployment/whoami --replicas=${WHOAMI_REPLICAS}
 
 .PHONY: whoami-restart
 whoami-restart:
-	kubectl -n whoami rollout restart deployment/whoami
+	kubectl -n demo rollout restart deployment/whoami
 
 .PHONY: whoami-status
 whoami-status:
 	@echo "======================================================================="
 	@echo "= whoami Network Resources:                                         ="
-	@echo "=   kubectl -n whoami get svc,endpoints,ingress -l app.kubernetes.io/name=whoami'    ="
+	@echo "=   kubectl -n demo get svc,endpoints,ingress -l app.kubernetes.io/name=whoami'    ="
 	@echo "======================================================================="
-	@kubectl -n whoami get svc -l 'app.kubernetes.io/name=whoami' -o custom-columns=NAME:.metadata.name,TYPE:.spec.type,CLUSTER-IP:.spec.clusterIP,EXTERNAL-IP:.spec.loadBalancerIP
+	@kubectl -n demo get svc -l 'app.kubernetes.io/name=whoami' -o custom-columns=NAME:.metadata.name,TYPE:.spec.type,CLUSTER-IP:.spec.clusterIP,EXTERNAL-IP:.spec.loadBalancerIP
 	@echo ""
-	@kubectl -n whoami get endpoints,ingress
+	@kubectl -n demo get endpoints,ingress
 	@echo "\n======================================================================="
 	@echo "= whoami Storage Resources:                                          ="
-	@echo "=   kubectl -n whoami get pvc -l 'app=whoami'                      ="
+	@echo "=   kubectl -n demo get pvc -l 'app=whoami'                      ="
 	@echo "======================================================================="
-	@kubectl -n whoami-multitool get pvc -l 'app.kubernetes.io/name=whoami' -o custom-columns=NAME:.metadata.name,STATUS:.status.phase,VOLUME:.spec.volumeName
+	@kubectl -n demo-multitool get pvc -l 'app.kubernetes.io/name=whoami' -o custom-columns=NAME:.metadata.name,STATUS:.status.phase,VOLUME:.spec.volumeName
 	@echo "\n======================================================================="
 	@echo "= whoami Deployment Resources:                                       ="
-	@echo "=   kubectl -n whoami get deployment,rs,pods -l 'app.kubernetes.io/name=whoami'       ="
+	@echo "=   kubectl -n demo get deployment,rs,pods -l 'app.kubernetes.io/name=whoami'       ="
 	@echo "======================================================================="
-	@kubectl -n whoami get deployment,rs,pods -l 'app.kubernetes.io/name=whoami'
+	@kubectl -n demo get deployment,rs,pods -l 'app.kubernetes.io/name=whoami'
