8a9caa5fa09e41cf12aa9a18726c3d6087265005
Matthew Stratton <mstratto@redhat.com>
2023-06-23 16:29:44 -0400

    ansible tuning

--- ansible/roles/sys/templates/motd.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-printf "\n"
-neofetch
-printf "\n"
-printf "Kubernetes\n"
-printf "\n"
-printf "  systemd:  /etc/systemd/system/k3s.service\n"
-printf "  logs:     journalctl -xeu k3s.service\n"
