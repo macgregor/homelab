7fe31c7102f27a5249297adfe8095c0c52eb3645
Matthew Stratton <mstratto@redhat.com>
2023-06-24 10:21:58 -0400

    upgrading metallb chart

--- kube/sys/metallb/metallb.mk
+++ kube/sys/metallb/metallb.mk
@@ -1,8 +1,22 @@
 
+
+
+.PHONY: metallb-deploy-first-time
+metallb-deploy-first-time:
+	helmfile --file ./sys/metallb/helmfile.yaml apply
+	kubectl apply -f ./sys/metallb/addresspool.yml
+
 .PHONY: metallb-deploy
 metallb-deploy:
-	helmfile --file ./sys/metallb/helmfile.yaml apply
+	SECRETKEY=$$(kubectl get secret --namespace "metallb" metallb-memberlist -o jsonpath="{.data.secretkey}" | base64 -d); \
+	helmfile --file ./sys/metallb/helmfile.yaml apply --set "speaker.secretValue=$${SECRETKEY}"
+	kubectl apply -f ./sys/metallb/addresspool.yml
 
 .PHONY: metallb-remove
 metallb-remove:
 	helmfile --file ./sys/metallb/helmfile.yaml destroy
+	kubectl delete -f ./sys/metallb/addresspool.yml
+
+.PHONY: metallb-restart
+metallb-restart:
+	kubectl -n metallb rollout restart deployment/adguard-home
