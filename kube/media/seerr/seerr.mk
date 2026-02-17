84cf32ff1fc7cb3e6c5ea12a8b7e638b6d93c1f6
Matthew Stratton <mstratto@redhat.com>
2026-02-17 17:51:05 -0500

    seerr deployment

---
+++ kube/media/seerr/seerr.mk
@@ -0,0 +1,54 @@
+SEERR_REPLICAS?=1
+
+.PHONY: seerr-deploy
+seerr-deploy:
+	kubectl apply -f ./media/seerr/namespace.yml
+	kubectl apply -f ./media/seerr/storage.yml
+	kubectl apply -f ./media/seerr/network.yml
+	kubectl apply -f ./media/seerr/seerr.yml
+
+.PHONY: seerr-remove
+seerr-remove:
+	-kubectl delete -f ./media/seerr/network.yml
+	-kubectl delete -f ./media/seerr/seerr.yml
+	-kubectl delete -f ./media/seerr/storage.yml
+
+.PHONY: seerr-debug
+seerr-debug:
+	kubectl -n media exec -it `kubectl -n media get pods -l app.kubernetes.io/name=seerr -o name` -- bash
+
+.PHONY: seerr-logs
+seerr-logs:
+	kubectl -n media logs -l app.kubernetes.io/name=seerr --follow --tail=50
+
+.PHONY: seerr-stop
+seerr-stop:
+	kubectl -n media scale deployment/seerr --replicas=0
+
+.PHONY: seerr-start
+seerr-start:
+	kubectl -n media scale deployment/seerr --replicas=${SEERR_REPLICAS}
+
+.PHONY: seerr-restart
+seerr-restart:
+	kubectl -n media rollout restart deployment/seerr
+
+.PHONY: seerr-status
+seerr-status:
+	@echo "======================================================================================"
+	@echo "= seerr Network Resources:                                                            ="
+	@echo "=   kubectl -n media get svc,endpoints,ingress -l app.kubernetes.io/name=seerr'        ="
+	@echo "======================================================================================"
+	@kubectl -n media get svc -l 'app.kubernetes.io/name=seerr' -o custom-columns=NAME:.metadata.name,TYPE:.spec.type,CLUSTER-IP:.spec.clusterIP,EXTERNAL-IP:.spec.loadBalancerIP
+	@echo ""
+	@kubectl -n media get endpoints,ingress
+	@echo "\n======================================================================================"
+	@echo "= seerr Storage Resources:                                                            ="
+	@echo "=   kubectl -n media get pvc -l 'app.kubernetes.io/name=seerr'                         ="
+	@echo "======================================================================================"
+	@kubectl -n media get pvc -l 'app.kubernetes.io/name=seerr' -o custom-columns=NAME:.metadata.name,STATUS:.status.phase,VOLUME:.spec.volumeName
+	@echo "\n======================================================================================"
+	@echo "= seerr Deployment Resources:                                                         ="
+	@echo "=   kubectl -n media get deployment,rs,pods -l 'app.kubernetes.io/name=seerr'          ="
+	@echo "======================================================================================"
+	@kubectl -n media get deployment,rs,pods -l 'app.kubernetes.io/name=seerr'
